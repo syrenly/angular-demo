b0b7557376ba69f95fe84e5cf35bc9c8ec0d69ce
syrenly <150293005+syrenly@users.noreply.github.com>
2024-02-12 22:43:03 +0100

    added unit tests

--- angular-w-tailwind/src/app/app.component.spec.ts
+++ angular-w-tailwind/src/app/app.component.spec.ts
@@ -15,7 +15,43 @@ describe("AppComponent", (): void => {
 		fixture.detectChanges();
 	});
 
-	it("should create", () => {
+	it("should create", (): void => {
 		expect(component).toBeTruthy();
+	});
+	it("should have the 'Tailwind' title", (): void => {
+		const compiled = fixture.nativeElement as HTMLElement;
+		const title = compiled.querySelector(".title")?.textContent;
+		expect(title).toEqual("Tailwind");
+	});
+	it("should render three buttons", (): void => {
+		const compiled = fixture.nativeElement as HTMLElement;
+		const buttons = compiled.querySelectorAll(".my-button");
+		expect(buttons.length).toBe(3);
+	});
+	describe("should test #onChangeAlertType ", (): void => {
+		it("case primary", (): void => {
+			const buttonSpy = spyOn(component, "onChangeAlertType");
+			const compiled = fixture.nativeElement as HTMLElement;
+			const buttons =
+				compiled.querySelectorAll<HTMLElement>(".my-button");
+			buttons[0].click();
+			expect(buttonSpy).toHaveBeenCalledWith("primary");
+		});
+		it("case danger", (): void => {
+			const buttonSpy = spyOn(component, "onChangeAlertType");
+			const compiled = fixture.nativeElement as HTMLElement;
+			const buttons =
+				compiled.querySelectorAll<HTMLElement>(".my-button");
+			buttons[1].click();
+			expect(buttonSpy).toHaveBeenCalledWith("danger");
+		});
+		it("case success", (): void => {
+			const buttonSpy = spyOn(component, "onChangeAlertType");
+			const compiled = fixture.nativeElement as HTMLElement;
+			const buttons =
+				compiled.querySelectorAll<HTMLElement>(".my-button");
+			buttons[2].click();
+			expect(buttonSpy).toHaveBeenCalledWith("success");
+		});
 	});
 });
