c7e61b59b5de9a55bb8ec938e0ca5443bc59ac2a
syrenly <150293005+syrenly@users.noreply.github.com>
2024-03-08 22:39:46 +0100

    updated todos

--- angular-demo/src/app/todo-list/todo-list.component.ts
+++ angular-demo/src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { ITodoModel } from "./todo-types";
-
+// Hint: save todos in browser storage
 @Component({
 	selector: "app-todo-list",
 	standalone: true,
 	imports: [CommonModule, FontAwesomeModule],
 	templateUrl: "./todo-list.component.html",
 	styleUrl: "./todo-list.component.scss",
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoListComponent {
 	/** Icon used to remove todos */
@@ -25,12 +26,15 @@ export class TodoListComponent {
 	 * @param input the html element that contains the name of todo. Once the todo is added, the input will be cleared
 	 */
 	addTodo(input: HTMLInputElement): void {
+		// do not add empty strings
+		if (!input.value || !input.value.trim()) return;
+		// Hint: implement reactive form to make a better check with error messages
 		const newTodo: ITodoModel = {
 			id: Date.now(),
 			title: input.value,
 			completed: false,
 		};
-		this.todos.push(newTodo);
+		this.todos = [...this.todos, newTodo];
 		input.value = "";
 	}
 	/**
@@ -38,16 +42,17 @@ export class TodoListComponent {
 	 * @param id f the todo
 	 */
 	removeTodo(id: number): void {
-		const index = this.todos.findIndex((todo): boolean => todo.id === id);
-		this.todos.splice(index, 1);
+		this.todos = this.todos.filter((todo): boolean => todo.id !== id);
 	}
 	/**
 	 * Method to switch the todo from positive to negative state and vice versa
 	 * @param id f the todo
 	 */
 	toggleTodo(id: number): void {
-		const index = this.todos.findIndex((todo): boolean => todo.id === id);
-		this.todos[index].completed = !this.todos[index].completed;
+		this.todos = this.todos.map(
+			(todo: ITodoModel): ITodoModel =>
+				todo.id === id ? { ...todo, completed: !todo.completed } : todo
+		);
 	}
 	/** Show the todos in the console */
 	saveAll(): void {
